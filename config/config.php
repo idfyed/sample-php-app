0a1a4b210aacc37d9773f4cf6c26b9f6b66090c0
Jonas Andreasson <jonas@diglias.com>
2017-09-04 23:53:52 +0200

    DIG-2212 * Uses attributes configured in the Acme version of the playground RP * Uses acme_loyaltyNuber in place of playground_string

--- config/config.php
+++ config/config.php
@@ -1,14 +1,20 @@
 <?php
 
-// The COMPANY_NAME is supplied by Diglias when a configuration for a 
+// The COMPANY_NAME is supplied by Diglias when a configuration for a
 // specific customer is set up in the Diglias Go system.
 define("COMPANY_NAME","playground",true);
 
-// The MAC_KEY is supplied by Diglias when a configuration for a 
-// specific customer is set up in the Diglias Go system. It shoule be 
-// used to verify the authenticity of all messages passed to and recived 
+// The MAC_KEY is supplied by Diglias when a configuration for a
+// specific customer is set up in the Diglias Go system. It shoule be
+// used to verify the authenticity of all messages passed to and recived
 // from the Diglias Go server.
 define("MAC_KEY","LW4eUhQkJfwJGgQU8JCT/g==",true);
 
 // Diglias backend environment to use for authentication.
-define("EAPI_ENDPOINT", Diglias\EAPI\Endpoint::ProdTest, true);
+define("EAPI_ENDPOINT", Diglias\EAPI\Endpoint::Test, true);
+
+// Key/Password used when accessing the RP Management API.
+define("RP_MANAGEMENT_SECRET", "LW4eUhQkJfwJGgQU8JCT/g==", true);
+
+// Endpoint used when accessing the RP Management API.
+define("RP_MANAGEMENT_ENDPOINT", Diglias\RPManagement\Endpoint::Test, true);
