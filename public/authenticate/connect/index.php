3dc033b2c4a8bbcb690416874b978e4236519e60
Jonas Andreasson <jonas@diglias.com>
2017-09-04 23:52:59 +0200

    DIG-2212: * Front end connect * Back end connect

---
+++ public/authenticate/connect/index.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Copyright 2017 (C) Diglias AB
+ *
+ * Prepare a message to the Diglias server and redirect the users
+ * browser to Diglias to ask the user to have a attribute added to
+ * their Diglias.
+ *
+ * @author jonas
+ *
+ */
+
+require '../../../vendor/autoload.php';
+require '../../../config/config.php';
+
+use Diglias\EAPI\RelyingParty;
+
+use sample\Util;
+
+// Generate a random request Id and store it in the session
+$requestId = Util::generateRandomString();
+session_start();
+$_SESSION['DigliasRequestId'] = $requestId;
+
+// A timestamp is required for a connect transaction
+$now = new DateTime("now", new DateTimeZone('UTC'));
+
+$params = array(
+    'auth_requestid' => $requestId,
+    'auth_returnlink' => Util::buildEndpointURL("/authenticate/success.php"),
+    'auth_cancellink' => Util::buildEndpointURL("/authenticate/cancel.php"),
+    'auth_rejectlink' => Util::buildEndpointURL("/authenticate/reject.php"),
+    'auth_timestamp' => $now->format(DATE_ATOM),
+
+    // Add the value from the form as a auth_rp_* attribute
+    // For this to work, the RP will have to be configured as a ambassador for the attribute.
+    "auth_rp_acme_loyaltyNumber" => $_POST["value"],
+
+    // Only show the connect attribute to the user, if this is omitted the user will be requested to give
+    // all attributes specified in the RP - this might seem illogical depending on use case.
+    "auth_attributes" => "acme_loyaltyNumber"
+);
+
+// Build the URL and redirect the users browser to it.
+$RP = new RelyingParty(COMPANY_NAME, MAC_KEY, EAPI_ENDPOINT);
+header('Location: ' . $RP->buildAuthnURL($params), true, 302);
---
+++ src/Diglias/RPManagement/Client.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * Copyright 2017 (C) Diglias AB
+ *
+ * @author jonas
+ *
+ * A class that wrapps communication to the Diglias RP Management API.
+ *
+ * The API specificaiton can be found @: https://test.diglias.com/doc-rp/rp-mgmt.jsp
+ *
+ */
+
+namespace Diglias\RPManagement;
+
+
+class Client
+{
+
+
+    /**
+     * Constructs a immutable client object that can communicate with the RPManagement API. The class relys on the curl
+     * module witch needs to be enabled.
+     *
+     * @param $companyName - The RP company name from the DIglias configuration
+     * @param $username - Username for authentication
+     * @param $secret - Password for authentication
+     * @param $endpoint - Endpoint select a value from RPManagement\Endpoint
+     */
+    public function __construct($companyName, $username, $secret, $endpoint)
+    {
+        $this->companyName = $companyName;
+        $this->username = $username;
+        $this->secret = $secret;
+        $this->endpoint = $endpoint;
+    }
+
+    /**
+     *
+     * Sends a request to the Diglias service to add a attribute value to the given users Diglias profile.
+     *
+     * @param $userId - ID of the user as returned in a eralier interaction where the user was authenticated by Diglias.
+     * @param $attributeName - Name of the attribute to add.
+     * @param $attributeValue - Value to add
+     * @return mixed - 204 on success, all other values indicate failure, the value is to be interpreted as a HTTP
+     * status code.
+     */
+    public function add($userId, $attributeName, $attributeValue)
+    {
+
+        $body = '{ "action": "ADD", "attributes": [ { "name": "' . $attributeName . '", "value" : "' . $attributeValue . '" } ] }';
+
+        return $this->sendRequest($userId, $body);
+    }
+
+
+    private function sendRequest($userid, $body)
+    {
+        $url = $this->endpoint . $this->companyName . '/' . $userid;
+
+        $headers = [
+            'Content-Type: application/json',
+            "Authorization: Basic " . base64_encode($this->username . ":" . $this->secret)
+        ];
+
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+        $result = curl_exec($ch);
+        $resultCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        curl_close($ch);
+
+        return $resultCode;
+
+    }
+
+    private $companyName;
+    private $username;
+    private $secret;
+    private $endpoint;
+}
